65eec2d8d356a785bb8effff761dd32cd4866442
DVDLejatszo <bertadavid31@gmail.com>
2025-11-04 17:35:27 +0100

    a

--- Assets/Scripts/Player/camera_pitch_follow.cs
+++ Assets/Scripts/Player/camera_pitch_follow.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class HeadFollowCamera : MonoBehaviour
 {
     [SerializeField] private Transform cameraTransform;
-    [SerializeField] private Transform eyeTransform;
+    [SerializeField] private Transform lefteyeTransform;
+    [SerializeField] private Transform righteyeTransform;
     [SerializeField] private Transform gunTransform;
-    //[SerializeField] private float maxHeadAngle = 60f; // korlát, hogy ne törjön ki a nyak :)
+    [SerializeField] private Transform cam_model_Transform;
+    [SerializeField] private float maxHeadAngle = 6f; // korlát, hogy ne törjön ki a nyak :)
     [SerializeField] private float followSpeed = 1f;  // smooth követés
 
     private void LateUpdate()
@@ -17,16 +19,26 @@ public class HeadFollowCamera : MonoBehaviour
         if (cameraPitch > 180) cameraPitch -= 360; // Unity rotációs fix
 
         // Limitáljuk a fej mozgását
-        //cameraPitch = Mathf.Clamp(cameraPitch, -maxHeadAngle, maxHeadAngle);
+        cameraPitch = Mathf.Clamp(cameraPitch, -maxHeadAngle, maxHeadAngle);
 
-        // Szem jelenlegi rotációja
-        Vector3 eyeEuler = eyeTransform.localEulerAngles;
-        eyeEuler.x = Mathf.LerpAngle(eyeEuler.x, cameraPitch, Time.deltaTime * followSpeed);
-        eyeTransform.localEulerAngles = eyeEuler;
+        // bal_szem jelenlegi rotációja
+        Vector3 lefteyeEuler = lefteyeTransform.localEulerAngles;
+        lefteyeEuler.x = Mathf.LerpAngle(lefteyeEuler.x, cameraPitch, Time.deltaTime * followSpeed);
+        lefteyeTransform.localEulerAngles = lefteyeEuler;
 
-        // Gun jelenlegi rotációja
+        // job_szem jelenlegi rotációja
+        Vector3 righteyeEuler = righteyeTransform.localEulerAngles;
+        righteyeEuler.x = Mathf.LerpAngle(righteyeEuler.x, cameraPitch, Time.deltaTime * followSpeed);
+        righteyeTransform.localEulerAngles = righteyeEuler;
+
+        // gun jelenlegi rotációja
         Vector3 gunEuler = gunTransform.localEulerAngles;
         gunEuler.x = Mathf.LerpAngle(gunEuler.x, cameraPitch, Time.deltaTime * followSpeed);
         gunTransform.localEulerAngles = gunEuler;
+
+        // cam_model jelenlegi rotációja
+        Vector3 cam_model_Euler = cam_model_Transform.localEulerAngles;
+        cam_model_Euler.x = Mathf.LerpAngle(cam_model_Euler.x, cameraPitch, Time.deltaTime * followSpeed);
+        cam_model_Transform.localEulerAngles = cam_model_Euler;
     }
 }
